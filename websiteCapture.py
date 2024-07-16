722608913cb1722bef1ce5ff988241ec3f66cd31
chiron <chirontch12@gmail.com>
2024-07-16 14:30:34 +0800

    Website Capture Program_version 1_with annotion

--- websiteCapture.py
+++ websiteCapture.py
@@ -4,15 +4,15 @@ import sys
 from PIL import Image
 from datetime import date
 
-sys.path.append("C:/Users/Switc/miniconda3/Lib/site-packages")
+sys.path.append("C:/Users/Switc/miniconda3/Lib/site-packages")                       # Change pt 1: install library location
 from selenium import webdriver
 import tkinter as tk
 from tkinter import messagebox
 
 def create_folder(folder_path, target_date):
     """Create a folder in the specified path"""
-    folder_name = os.path.join(folder_path, target_date)
-    os.makedirs(folder_name, exist_ok=True)
+    folder_name = os.path.join(folder_path, target_date)                            # Change pt 2: create a folder named by today 's date
+    os.makedirs(folder_name, exist_ok=True)                                          
     return folder_name
 
 def take_screenshot(url, folder_path, target_date, i):
@@ -20,20 +20,20 @@ def take_screenshot(url, folder_path, target_date, i):
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(5)
-    screenshot_file_name = f"賽馬賠率-{target_date}_場次{i}.png"                        # renaming to screencap.png
+    screenshot_file_name = f"賽馬賠率-{target_date}_場次{i}.png"                      # Change pt 3: The output name can be change
     screenshot_path = os.path.join(folder_path, target_date, screenshot_file_name)
     driver.save_screenshot(screenshot_path)
     driver.quit()
     return screenshot_path
 
-def capture_screenshots(folder_path, target_date):
+def capture_screenshots(folder_path, target_date):                                   # Change pt 4: if website URL have more than 1 , it can be adjust
     """Capture screenshots for all stages (1 to 11)"""
     for i in range(11, 0, -1):
         url = f"https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=ch&date={target_date}&venue=ST&raceno={i}"
         take_screenshot(url, folder_path, target_date, i)
 
 def main():
-    folder_path = "G:\Mother\SynologyDrive\\4_Mum_個人\\6_JockeyClub"
+    folder_path = "G:\Mother\SynologyDrive\\4_Mum_個人\\6_JockeyClub"                # Change pt 5: Output location
     current_date = date.today()
     target_date = current_date.strftime("%Y-%m-%d")
 
