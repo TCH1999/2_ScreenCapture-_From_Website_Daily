82b5045bcad692abd34d408b5eb8347ae41a8cf8
chiron <chirontch12@gmail.com>
2024-07-17 17:33:08 +0800

    git version update_Re1

--- websiteCapture.py
+++ websiteCapture.py
@@ -59,6 +59,5 @@ def main(url,debug):
 if __name__ == "__main__":
    debug = True
    file = open(r'G:\SynologyDrive\2_LifeGoalProject\2_programmingProject\1_python\3_codeProject\1_automation_Website_Capture\DAILY_WEBSITE_CAPTURE\taskLog','a')
-   file.write(f'{datetime.datetime.now()} - Th
-               e script ran \n') 
+   file.write(f'{datetime.datetime.now()} - The script ran \n') 
    main(debug)
