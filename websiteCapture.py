eab76dce0243a2d570797b9a09baf2f015278795
chiron <chirontch12@gmail.com>
2024-07-17 16:46:43 +0800

    Auto

--- websiteCapture.py
+++ websiteCapture.py
@@ -53,6 +53,7 @@ def main():
 
 if __name__ == "__main__":
     file = open(r'G:\SynologyDrive\2_LifeGoalProject\2_programmingProject\1_python\3_codeProject\1_automation_Website_Capture\DAILY_WEBSITE_CAPTURE\taskLog','a')
-    file.write(f'{datetime.datetime.now()} - The script ran \n')
+    file.write(f'{datetime.datetime.now()} - Th
+               e script ran \n')
     main()
     
